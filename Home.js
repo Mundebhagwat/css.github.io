b12d50bde795dffe8c6ddc6db6c0d47d4ca936c8
Mundebhagwat <123530941+Mundebhagwat@users.noreply.github.com>
2024-03-27 23:43:23 +0530

    new files

--- Home.js
+++ Home.js
@@ -13,6 +13,13 @@ document.getElementById("registeration").addEventListener("click", function(){
         window.location.href = "registration.html";
     } ,500)
 })
+// dashboard
+document.getElementById("dashboard").addEventListener("click", function(){
+    document.getElementById('loading').style.display = 'block';
+    setTimeout(function(){
+        window.location.href = "crops.html";
+    } ,500)
+})
 // burger
 function toggleMenu() {
     let menu = document.querySelector('.burger-menu');
